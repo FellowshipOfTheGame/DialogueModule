d82e28e58d73b52a15fe6ae4d43969b106e52921
Ítalo <italo.ts4895@gmail.com>
2022-04-19 19:29:19 -0300

    Move package folders to subfolder

---
+++ Runtime/OptionsDialogue.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Fog.Dialogue {
+    /// <summary>
+    ///     Creates a scriptable object for an array of dialogue lines, so that it can be saved as a file.
+    /// </summary>
+    [CreateAssetMenu(fileName = "NewOptionsDialogue", menuName = "FoG/DialogueModule/OptionsDialogue")]
+    public class OptionsDialogue : Dialogue {
+        [SerializeField] private DialogueLine question;
+        [SerializeField] private DialogueOptionInfo[] options;
+
+        public override void AfterDialogue() {
+            base.AfterDialogue();
+            Agent.Instance.BlockInteractions();
+            DialogueHandler.instance.DisplayOptions(question, options);
+            DialogueHandler.instance.OnDialogueEnd -= AfterDialogue;
+        }
+    }
+}
